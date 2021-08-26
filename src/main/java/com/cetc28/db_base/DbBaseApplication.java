d9fc7957ef0097ce10ff4d14253d44eda3b5543a
Hymn1218 <huangxiang@pku.edu.cn>
2021-08-26 16:30:41 +0800

    更新项目 2021/08/26

---
+++ src/main/java/com/cetc28/db_base/DbBaseApplication.java
@@ -0,0 +1,13 @@
+package com.cetc28.db_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbBaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cetc28/db_base/DbBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cetc28.db_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
